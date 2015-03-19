b0172746f775dc968909808394e517fc8eea6d93
bdosari <aldawsar004@knights.gannon.edu>
2015-03-19 01:48:38 -0400

    test error

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("adder = " + Adder.getNumber());
+		System.out.println("adder = " ++ Adder.getNumber());
 	}
 
 }
